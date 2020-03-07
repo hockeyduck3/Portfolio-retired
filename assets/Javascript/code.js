84257446af9c3cc256cad279469ea7ab5e1ce9bd
LJ Spencer <hockeyduck3@gmail.com>
2020-03-06 20:18:00 -0700

    Cleaned up the file system a little bit, and put the JS for the Hello there easter egg on it's own file.

---
+++ assets/Javascript/code.js
@@ -0,0 +1,4 @@
+document.getElementById("btn1").addEventListener("click", function(){
+    document.getElementById("Hello").src = "./assets/images/giphy.gif";
+    document.getElementById("Hello").alt = "Hello there!";
+})
