1822be559453e101fec8306dcd4c5209ce852b7f
LJ Spencer <hockeyduck3@gmail.com>
2020-03-07 19:59:52 -0700

    Changed the alt for the hello there easter egg

--- assets/Javascript/code.js
+++ assets/Javascript/code.js
@@ -1,4 +1,4 @@
 document.getElementById("btn1").addEventListener("click", function(){
     document.getElementById("Hello").src = "./assets/images/giphy.gif";
-    document.getElementById("Hello").alt = "Hello there!";
+    document.getElementById("Hello").alt = "Obi-Wan Kenobi says, hello there!";
 })
