144b3cf7e65a531f8ae2b0d9ef36a2b6b9007397
LJ Spencer <hockeyduck3@gmail.com>
2020-03-09 15:30:27 -0600

    Added a setTimeout onto the JS

--- assets/Javascript/code.js
+++ assets/Javascript/code.js
@@ -1,4 +1,9 @@
 document.getElementById("btn1").addEventListener("click", function(){
     document.getElementById("Hello").src = "./assets/images/giphy.gif";
     document.getElementById("Hello").alt = "Obi-Wan Kenobi says, hello there!";
+
+    setTimeout(function(){ 
+        document.getElementById("Hello").src = "./assets/images/Snapchat-1637716371.jpg";
+        document.getElementById("Hello").alt = "LJ holding his sister's dog Teeny";
+    }, 2800);
 })
