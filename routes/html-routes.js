2c25cce8a4efaf78ee2a01c5b954114db9a2e3b8
LJ Spencer <hockeyduck3@gmail.com>
2020-06-24 03:29:29 -0600

    Added the rest of the handlebars and added the rest of the routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -7,6 +8,29 @@ router.get('/', (req, res) => {
     }
 
     res.render('index', pageTitle);
-})
+});
+
+router.get('/portfolio', (req, res) => {
+    const pageTitle = {
+        title: 'Portfolio'
+    }
+
+    res.render('portfolio', pageTitle);
+});
+
+router.get('/contact', (req, res) => {
+    const pageTitle = {
+        title: 'Contact'
+    }
+
+    res.render('contact', pageTitle);
+});
+
+router.get('/resume', (req, res) => {
+    fs.readFile('LJ-Spencer-Resume-2020.pdf', function (err, data){
+      res.contentType('application/pdf');
+      res.send(data);
+    });
+});
 
 module.exports = router;
